293bbe5dd33e9215b6bc13cadf0e872a277fb32e
Vladislav Yakovlev <vlad309523@gmail.com>
2018-02-06 19:48:43 +0300

    FEAT: reserve prediction

--- catboost/fit.sh
+++ catboost/fit.sh
@@ -1,7 +1,1 @@
-catboost fit \
-	-f train.tsv \
-	-t test.tsv \
-	--cd columns.cd \
-	-i 10000 \
-        -r 483625340 \
-	--verbose \
+catboost fit -f train.tsv -t test.tsv --cd columns.cd -i 10000 -r 483625340
